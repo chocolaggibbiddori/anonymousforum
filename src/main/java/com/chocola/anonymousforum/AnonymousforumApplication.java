021b203b23cadaa6b71e951aabd9af3a43bface9
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2023-09-11 19:54:51 +0900

    First Commit

---
+++ src/main/java/com/chocola/anonymousforum/AnonymousforumApplication.java
@@ -0,0 +1,13 @@
+package com.chocola.anonymousforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonymousforumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chocola/anonymousforum/AnonymousforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocola.anonymousforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
